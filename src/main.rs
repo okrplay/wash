7f255fe4a61712b82eb6ee76df276dc9e1062359
okrplay <32576280+okrplay@users.noreply.github.com>
2020-11-18 15:39:54 +0100

    fix: recurse more than one folder deep

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod pattern;
 
 use clap::clap_app;
 use pattern::{PatternVec, Patterns};
-use std::fs::{read_dir, remove_dir_all, remove_file};
+use std::fs::{read_dir, remove_dir_all, remove_file, ReadDir};
 
 fn main() {
     let matches = clap_app!(wash =>
@@ -17,8 +17,12 @@ fn main() {
     let mut patterns = PatternVec::new();
     patterns.load_default();
 
-    let dir = read_dir(matches.value_of("DIRECTORY").unwrap())
+    let mut dir = read_dir(matches.value_of("DIRECTORY").unwrap())
         .expect("error: provided directory is not a directory");
+    check_and_delete(&mut dir, &patterns)
+}
+
+fn check_and_delete(dir: &mut ReadDir, patterns: &PatternVec) {
     for entry in dir {
         let entry = entry.unwrap();
         let file_type = entry.file_type().unwrap();
@@ -28,6 +32,8 @@ fn main() {
             } else if file_type.is_dir() {
                 remove_dir_all(entry.path()).unwrap();
             }
+        } else if file_type.is_dir() {
+            check_and_delete(&mut read_dir(entry.path()).unwrap(), patterns)
         }
     }
 }
